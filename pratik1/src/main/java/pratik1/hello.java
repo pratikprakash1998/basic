8da1ec7d14d856ad6ee88119972bddf365dfc16f
PR20167953 <PR20167953@10.218.32.149>
2021-03-02 16:55:32 +0530

    first commit

---
+++ pratik1/src/main/java/pratik1/hello.java
@@ -0,0 +1,9 @@
+package pratik1;
+
+public class hello {
+	public static void main(String[] args){ 
+        System.out.println("Hello World"); 
+	
+
+}
+}
---
+++ pratik1/src/test/java/com/springrest/spring/mongodb/api/Pratik1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.spring.mongodb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratik1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
